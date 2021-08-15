898d62b5118d8efbc914a2c12bdd17f4112b549d
gela samsonidze <samosnidze7@yahoo.com>
2021-08-15 12:11:29 +0400

    add mobile detection

--- js/index.js
+++ js/index.js
@@ -1,27 +1,53 @@
-VanillaTilt.init(document.querySelectorAll(".card"), {
-    reverse:                false,  // reverse the tilt direction
-    max:                    10,     // max tilt rotation (degrees)
-    startX:                 0,      // the starting tilt on the X axis, in degrees.
-    startY:                 0,      // the starting tilt on the Y axis, in degrees.
-    perspective:            1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:                  1.05,      // 2 = 200%, 1.5 = 150%, etc..
-    speed:                  300,    // Speed of the enter/exit transition
-    transition:             true,   // Set a transition on enter/exit.
-    axis:                   null,   // What axis should be disabled. Can be X or Y.
-    reset:                  true,    // If the tilt effect has to be reset on exit.
-    easing:                 "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-    glare:                  false,   // if it should have a "glare" effect
-    "max-glare":            0.3,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
-    "glare-prerender":      false,  // false = VanillaTilt creates the glare elements for you, otherwise
-                                    // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
-    "mouse-event-element":  null,    // css-selector or link to HTML-element what will be listen mouse events
-                                    // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
-    gyroscope:              true,    // Boolean to enable/disable device orientation detection,
-    gyroscopeMinAngleX:     -45,     // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
-    gyroscopeMaxAngleX:     45,      // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
-    gyroscopeMinAngleY:     -45,     // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
-    gyroscopeMaxAngleY:     45,      // This is the top limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the bottom border of the element;
-});
+const initTiltEffect = () => {
+    VanillaTilt.init(document.querySelectorAll(".card"), {
+        reverse:                false,  // reverse the tilt direction
+        max:                    10,     // max tilt rotation (degrees)
+        startX:                 0,      // the starting tilt on the X axis, in degrees.
+        startY:                 0,      // the starting tilt on the Y axis, in degrees.
+        perspective:            1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale:                  1.05,      // 2 = 200%, 1.5 = 150%, etc..
+        speed:                  300,    // Speed of the enter/exit transition
+        transition:             true,   // Set a transition on enter/exit.
+        axis:                   null,   // What axis should be disabled. Can be X or Y.
+        reset:                  true,    // If the tilt effect has to be reset on exit.
+        easing:                 "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        glare:                  false,   // if it should have a "glare" effect
+        "max-glare":            0.3,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+        "glare-prerender":      false,  // false = VanillaTilt creates the glare elements for you, otherwise
+                                        // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+        "mouse-event-element":  null,    // css-selector or link to HTML-element what will be listen mouse events
+                                        // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+        gyroscope:              true,    // Boolean to enable/disable device orientation detection,
+        gyroscopeMinAngleX:     -45,     // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+        gyroscopeMaxAngleX:     45,      // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+        gyroscopeMinAngleY:     -45,     // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+        gyroscopeMaxAngleY:     45,      // This is the top limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the bottom border of the element;
+    });
+}
+
+const regex = /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+console.log(navigator.userAgent);
+
+if(regex.test(navigator.userAgent) ) {
+    console.log("mobile");
+
+    const span = document.createElement("span");
+    span.textContent = "this is mobile";
+
+    document.querySelector(".attribution").appendChild(span);
+    
+}else{
+    console.log("pc");
+
+    const span = document.createElement("span");
+    span.textContent = "this is pc";
+
+    document.querySelector(".attribution").appendChild(span);
+    initTiltEffect();
+}
+
+
 
 
 /************************\
@@ -146,7 +172,6 @@ const initParticles = () => {
         PARTICLES_ARRAY.pop();
     }
 
-    console.log(PARTICLES_ARRAY);
 
     for(let i = 0; i < NUMBER_OF_PARTICLES; i++){
         const x = edgeSize + Math.floor((Math.random()*(canvas.width - 2*edgeSize)));
